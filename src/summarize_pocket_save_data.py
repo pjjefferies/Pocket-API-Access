d85fbef05807fd57e25d23cfb332942a9beef859
Paul Jefferies <jefferie@umich.edu>
2023-11-30 00:45:41 -0500

    summarize_pocket_save_data module updated to have two stages of data summarizing

--- src/summarize_pocket_save_data.py
+++ src/summarize_pocket_save_data.py
@@ -1,141 +1,116 @@
-from typing import Callable
+""" Get saved data in data folder and show summary over time of unread and archive articles in text and graphic forms """
+
+import os
+import datetime as dt
+
 import pandas as pd
 
+from src.summarize_pocket_archived_articles import summarize_pocket_archived_articles
+from src.summarize_pocket_unread_articles import summarize_pocket_unread_articles
 from src.config.config_logging import logger
 from src.config.config_main import cfg
 
 
-def summarize_pocket_archived_articles(filename: str) -> pd.DataFrame:
-    pocket_save_data: pd.DataFrame = pd.read_csv(filepath_or_buffer=filename)
+def summarize_pocket_save_data_stage_1() -> (
+    tuple[dict[str, pd.DataFrame], dict[str, int]]
+):
+    """
+    Get Saved Data in data folder and save archived and unread summaries in dictionary of DataFrames with datetimes strings as keys
+    """
 
-    pocket_save_data["time_added_dt"] = pd.to_datetime(
-        pocket_save_data["time_added"], unit="s"
-    )
-    pocket_save_data["year_added"] = pocket_save_data["time_added_dt"].dt.year
+    # Get summary data from saved data in data folder
+    saved_files: list[list[str]] = [
+        [filename, filename[13:-4]]
+        for filename in os.listdir(cfg.DATA.FOLDER)
+        if filename.startswith(cfg.DATA.FILE_PREFIX)
+    ]
 
-    pocket_save_data["time_read_dt"] = pd.to_datetime(
-        pocket_save_data["time_read"], unit="s"
-    )
-    pocket_save_data["year_read"] = pocket_save_data["time_read_dt"].dt.year
+    archive_summary_data: dict[str, pd.DataFrame] = {}
+    unread_summary_data: dict[str, int] = {}
 
-    first_year_added: int = pocket_save_data["year_added"].min()
-    last_year_added: int = pocket_save_data["year_added"].max()
+    for filename, file_dt in saved_files:
+        filepath: str = os.path.join(cfg.DATA.FOLDER, filename)
+        archive_data: pd.DataFrame = summarize_pocket_archived_articles(filepath)
+        if not archive_data.empty:
+            archive_summary_data[file_dt] = archive_data
 
-    first_year_read: int = pocket_save_data[pocket_save_data["time_read"] != 0][
-        "year_read"
-    ].min()
-    last_year_read: int = pocket_save_data["year_read"].max()
+        unread_count: int
+        _, unread_count = summarize_pocket_unread_articles(filepath)
+        unread_summary_data[file_dt] = unread_count
 
-    pocket_data_archived_summary = pd.DataFrame(
-        {}, columns=range(first_year_added, last_year_added + 1)
-    )
-    for read_year in range(first_year_read, last_year_read + 1):
-        this_read_year_by_saved_year: "pd.Series[int]" = pd.Series([])
-        for added_year in range(first_year_added, last_year_added + 1):
-            this_year_added_count: int = pocket_save_data[
-                (pocket_save_data["year_added"] == added_year)
-                & (pocket_save_data["year_read"] == read_year)
-                & (pocket_save_data["status"] == 1)
-            ].shape[0]
+    return archive_summary_data, unread_summary_data
 
-            this_read_year_saved_year: pd.Series = pd.Series(
-                [this_year_added_count], index=[added_year]
-            )
-            if this_read_year_by_saved_year.shape[0] == 0:
-                this_read_year_by_saved_year = this_read_year_saved_year
-            else:
-                this_read_year_by_saved_year = pd.concat(
-                    [
-                        this_read_year_by_saved_year,
-                        this_read_year_saved_year,
-                    ]
-                )
-        this_read_year_by_saved_year = pd.concat(
+
+def summarize_pocket_save_data_stage_2(
+    archive_summary_data: dict[str, pd.DataFrame], unread_summary_data: dict[str, int]
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Create new DataFrame with total read per year and unready by datetime of data"""
+    # Archive Data
+    #     First get previous year archive totals
+    this_year: int = dt.datetime.now().year
+    latest_archive_data_date: str = dt.datetime.strftime(
+        max(
             [
-                this_read_year_by_saved_year,
-                pd.Series([read_year], index=["read_year"]),
+                dt.datetime.strptime(datetime_str, cfg.APP.DATETIME_FORMAT)
+                for datetime_str in archive_summary_data
             ]
-        )
-
-        isint: Callable([Any], bool) = lambda x: isinstance(x, int)
-        years_filter: pd.Series[bool] = pd.Series(
-            this_read_year_by_saved_year.index
-        ).apply(isint)
-        years_filter.index = this_read_year_by_saved_year.index
-        this_read_year_by_saved_year_years_only: pd.Series[
-            int
-        ] = this_read_year_by_saved_year.loc[years_filter]
-        total_read_this_year: int = int(this_read_year_by_saved_year_years_only.sum())
-
-        this_read_year_by_saved_year = pd.concat(
-            [
-                this_read_year_by_saved_year,
-                pd.Series([total_read_this_year], index=["Total"]),
-            ]
-        )
-
-        pocket_data_archived_summary = pd.concat(
-            [pocket_data_archived_summary, this_read_year_by_saved_year.to_frame().T]
-        )
-
-    pocket_data_archived_summary = pocket_data_archived_summary.astype("int")
-
-    pocket_data_archived_summary.set_index("read_year", inplace=True)
-
-    return pocket_data_archived_summary
-
-
-def summarize_pocket_unread_articles(filename: str) -> tuple[pd.DataFrame, int]:
-    pocket_save_data: pd.DataFrame = pd.read_csv(filepath_or_buffer=filename)
-
-    pocket_save_data["time_added_dt"] = pd.to_datetime(
-        pocket_save_data["time_added"], unit="s"
+        ),
+        cfg.APP.DATETIME_FORMAT,
     )
-    pocket_save_data["year_added"] = pocket_save_data["time_added_dt"].dt.year
+    last_saved_archive_data: pd.DataFrame = archive_summary_data[
+        latest_archive_data_date
+    ]
 
-    pocket_save_data["time_read_dt"] = pd.to_datetime(
-        pocket_save_data["time_read"], unit="s"
+    archive_dates: pd.Series = pd.Series(
+        [
+            dt.datetime(year, 1, 1, 12)
+            for year in last_saved_archive_data.index
+            if year != dt.datetime.now().year
+        ]
     )
-    pocket_save_data["year_read"] = pocket_save_data["time_read_dt"].dt.year
 
-    first_year_added: int = pocket_save_data["year_added"].min()
-    last_year_added: int = pocket_save_data["year_added"].max()
+    archive_counts: pd.Series = pd.Series(
+        [
+            last_saved_archive_data.at[year, "Total"]
+            for year in last_saved_archive_data.index
+            if year != dt.datetime.now().year
+        ]
+    )
 
-    first_year_read: int = pocket_save_data[pocket_save_data["time_read"] != 0][
-        "year_read"
-    ].min()
-    last_year_read: int = pocket_save_data["year_read"].max()
+    #     Second get this year archive history
+    this_year_saved_archive_totals_dict: dict[str, int] = {
+        datetime_str: archive_summary_data[datetime_str].at[
+            int(datetime_str[:4]), "Total"
+        ]
+        for datetime_str in archive_summary_data
+        if datetime_str[:4] == str(this_year)
+    }
+    this_year_saved_archive_totals: pd.DataFrame = pd.DataFrame(
+        data=this_year_saved_archive_totals_dict.values(),
+        index=this_year_saved_archive_totals_dict.keys(),
+        columns=["Totals"],
+    )
 
-    pocket_data_unread_by_year: pd.Series[int] = pd.Series([])
-    for added_year in range(first_year_added, last_year_added + 1):
-        this_year_added_count: int = pocket_save_data[
-            (pocket_save_data["year_added"] == added_year)
-            & (pocket_save_data["status"] == 0)
-        ].shape[0]
+    archive_dates = pd.concat(
+        [archive_dates, pd.to_datetime(pd.Series(this_year_saved_archive_totals.index))]
+    )
+    archive_counts = pd.concat(
+        [archive_counts, this_year_saved_archive_totals["Totals"]]
+    )
 
-        this_read_year_saved_year: pd.Series[int] = pd.Series(
-            [this_year_added_count], index=[added_year]
-        )
-        if pocket_data_unread_by_year.shape[0] == 0:
-            pocket_data_unread_by_year = this_read_year_saved_year
-        else:
-            pocket_data_unread_by_year = pd.concat(
-                [
-                    pocket_data_unread_by_year,
-                    this_read_year_saved_year,
-                ]
-            )
+    new_archive_data: pd.DataFrame = pd.DataFrame(
+        {"archive_date": list(archive_dates), "archive_count": list(archive_counts)}
+    )
 
-    unread_article_count: int = pocket_data_unread_by_year.sum()
+    # Unread Data
+    unread_dates: list[str] = list(unread_summary_data.keys())
+    unread_counts: list[int] = list(unread_summary_data.values())
 
-    return pocket_data_unread_by_year, unread_article_count
+    new_unread_data: pd.DataFrame = pd.DataFrame(
+        {"check_date": unread_dates, "unread_count": unread_counts}
+    )
+    new_unread_data["check_date"] = pd.to_datetime(
+        arg=new_unread_data["check_date"], format=cfg.APP.DATETIME_FORMAT
+    )
 
-
-if __name__ == "__main__":
-    filename: str = ".\\data\\Pocket_Saves_2023-11-24T221010.csv"
-    archive_data: pd.DataFrame = summarize_pocket_archived_articles(filename=filename)
-    unread_data, unread_count = summarize_pocket_unread_articles(filename=filename)
-
-    print(f"\nArchive Data\n{archive_data}")
-    print(f"\nUnread Data\n{unread_data}")
-    print(f"\nUnread Count: {unread_count:,.0f}")
+    return new_archive_data, new_unread_data
